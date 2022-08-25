14c19d25a5abae6c29b7b2a5712966e5ed21ef15
ErickNMA <ericknathancoro@hotmail.com>
2022-08-24 21:54:00 -0300

    .

--- semaforo.ino
+++ semaforo.ino
@@ -17,8 +17,36 @@ void setup()
     digitalWrite(LED_GREEN, LOW);
 }
 
+int mode=0;
+
+int td = 200;
+unsigned long t=0;
+
 //Roda em loop infinito e executa as tarefas desejadas:
 void loop()
 {
+    switch(mode%3)
+    {
+        case 0:
+            digitalWrite(LED_RED, HIGH);
+            digitalWrite(LED_YELLOW, LOW);
+            digitalWrite(LED_GREEN, LOW);
+        break;
+        case 1:
+            digitalWrite(LED_RED, LOW);
+            digitalWrite(LED_YELLOW, HIGH);
+            digitalWrite(LED_GREEN, LOW);
+        break;
+        case 2:
+            digitalWrite(LED_RED, LOW);
+            digitalWrite(LED_YELLOW, LOW);
+            digitalWrite(LED_GREEN, HIGH);
+        break;
+    }
 
+    if(((millis() - t) >= td) && (digitalRead(BUTTON) == HIGH))
+    {
+        t = millis();
+        mode++;
+    }
 }
